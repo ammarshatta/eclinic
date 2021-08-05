c663847c0d28f4408ef7465586956975e39236e7
ammarshatta <43485980+ammarshatta@users.noreply.github.com>
2021-08-06 01:34:11 +0200

    rm includes

--- hooks/AfterPluginInstall.js
+++ hooks/AfterPluginInstall.js
@@ -4,7 +4,7 @@ const stat = util.promisify(fs.stat);
 
 module.exports = function(ctx) {
     // Make sure android platform is part of build
-    if (!ctx.opts.platforms.includes('android')) return;
+   
 
     const pluginPathInPlatformIosDir = context.opts.projectRoot + '/Plugins/' + context.opts.plugin.id +"/src/ios/JabberGuest.framework/Versions/A/JabberGuest";
 
