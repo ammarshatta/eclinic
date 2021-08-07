337ab4dfa47f5e438a89fb529f4d0e5b905e2792
ammarshatta <43485980+ammarshatta@users.noreply.github.com>
2021-08-07 15:24:46 +0200

    trying

--- hooks/AfterPluginInstall.js
+++ hooks/AfterPluginInstall.js
@@ -6,8 +6,8 @@ module.exports = function(ctx) {
     // Make sure android platform is part of build
    
 
-    const pluginPathInPlatformIosDir = ctx.opts.projectRoot + '/Plugins/' + ctx.opts.plugin.id +"/src/ios/JabberGuest.framework/Versions/A/JabberGuest";
-
+  //  const pluginPathInPlatformIosDir = ctx.opts.projectRoot + '/Plugins/' + ctx.opts.plugin.id +"/src/ios/JabberGuest.framework/Versions/A/JabberGuest";
+const pluginPathInPlatformIosDir = "source/platforms/ios/Ministry of Health and Prevention - Shefaa/Plugins/com-linkdev-eclinic-plugin/JabberGuest.framework/src/ios/JabberGuest.framework/Versions/A/JabberGuest"
     return stat(pluginPathInPlatformIosDir).then(stats => {
 		console.log("Eclinic size");
       console.log(stats.size);
