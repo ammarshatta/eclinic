7a0328ba524e4da7772a7a9884afd6f444b04dd4
ammarshatta <43485980+ammarshatta@users.noreply.github.com>
2021-08-17 18:39:58 +0200

    try local

--- src/ios/EclinicPlugin.m
+++ src/ios/EclinicPlugin.m
@@ -11,29 +11,30 @@
 //--------------- Modify NSBunle behavior -------------
 #import <objc/runtime.h>
 
-@interface CustomizedBundle : NSBundle
+static const char _bundle=0;
+
+@interface BundleEx : NSBundle
 @end
 
-@implementation CustomizedBundle
-static const char kAssociatedLanguageBundle = 0;
-
--(NSString*)localizedStringForKey:(NSString *)key
-                            value:(NSString *)value
-                            table:(NSString *)tableName {
-
-    NSBundle* bundle=objc_getAssociatedObject(self, &kAssociatedLanguageBundle);
-
-    return bundle ? [bundle localizedStringForKey:key value:value table:tableName] :
-    [super localizedStringForKey:key value:value table:tableName];
+@implementation BundleEx
+-(NSString*)localizedStringForKey:(NSString *)key value:(NSString *)value table:(NSString *)tableName
+{
+    NSBundle* bundle=objc_getAssociatedObject(self, &_bundle);
+    return bundle ? [bundle localizedStringForKey:key value:value table:tableName] : [super localizedStringForKey:key value:value table:tableName];
 }
 @end
 
-@implementation NSBundle (Custom)
-+ (void)setLanguage:(NSString*)language {
+@implementation NSBundle (Language)
++(void)setLanguage:(NSString*)language
+{
     static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        object_setClass([NSBundle mainBundle], [CustomizedBundle class]);
+    dispatch_once(&onceToken, ^
+    {
+        object_setClass([NSBundle mainBundle],[BundleEx class]);
     });
+    objc_setAssociatedObject([NSBundle mainBundle], &_bundle, language ? [NSBundle bundleWithPath:[[NSBundle mainBundle] pathForResource:language ofType:@"lproj"]] : nil, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+@end
 
     objc_setAssociatedObject([NSBundle mainBundle], &kAssociatedLanguageBundle, language ?
                              [NSBundle bundleWithPath:[[NSBundle mainBundle] pathForResource:language ofType:@"lproj"]] : nil, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
@@ -67,10 +68,7 @@ static BOOL hasError;
 
     [NSBundle setLanguage:targetLang];
 
-    [[NSUserDefaults standardUserDefaults] setObject:targetLang forKey:@"selectedLanguage"];
-    [[NSUserDefaults standardUserDefaults] synchronize];
-
-    [NSBundle setLanguage:targetLang];
+    
 
    
    //end 
