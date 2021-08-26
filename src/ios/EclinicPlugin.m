8982adf608bdb1e525443cd2b17c387ff4b5e7c1
ammarshatta <43485980+ammarshatta@users.noreply.github.com>
2021-08-26 14:04:14 +0200

    try to reload main controller

--- src/ios/EclinicPlugin.m
+++ src/ios/EclinicPlugin.m
@@ -9,37 +9,39 @@
 #import <AVFoundation/AVFoundation.h>
 
 //--------------- Modify NSBunle behavior -------------
+#import <Foundation/Foundation.h>
 
-// @interface NSBundle (Language)
-// +(void)setLanguage:(NSString*)language;
-// @end
 
-// #import <objc/runtime.h>
+@interface NSBundle (Language)
++(void)setLanguage:(NSString*)language;
+@end
 
-// static const char _bundle=0;
+#import <objc/runtime.h>
 
-// @interface BundleEx : NSBundle
-// @end
+static const char _bundle=0;
 
-// @implementation BundleEx
-// -(NSString*)localizedStringForKey:(NSString *)key value:(NSString *)value table:(NSString *)tableName
-// {
-    // NSBundle* bundle=objc_getAssociatedObject(self, &_bundle);
-    // return bundle ? [bundle localizedStringForKey:key value:value table:tableName] : [super localizedStringForKey:key value:value table:tableName];
-// }
-// @end
+@interface BundleEx : NSBundle
+@end
 
-// @implementation NSBundle (Language)
-// +(void)setLanguage:(NSString*)language
-// {
-    // static dispatch_once_t onceToken;
-    // dispatch_once(&onceToken, ^
-    // {
-        // object_setClass([NSBundle mainBundle],[BundleEx class]);
-    // });
-    // objc_setAssociatedObject([NSBundle mainBundle], &_bundle, language ? [NSBundle bundleWithPath:[[NSBundle mainBundle] pathForResource:language ofType:@"lproj"]] : nil, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-// }
-// @end
+@implementation BundleEx
+-(NSString*)localizedStringForKey:(NSString *)key value:(NSString *)value table:(NSString *)tableName
+{
+    NSBundle* bundle=objc_getAssociatedObject(self, &_bundle);
+    return bundle ? [bundle localizedStringForKey:key value:value table:tableName] : [super localizedStringForKey:key value:value table:tableName];
+}
+@end
+
+@implementation NSBundle (Language)
++(void)setLanguage:(NSString*)language
+{
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^
+    {
+        object_setClass([NSBundle mainBundle],[BundleEx class]);
+    });
+    objc_setAssociatedObject([NSBundle mainBundle], &_bundle, language ? [NSBundle bundleWithPath:[[NSBundle mainBundle] pathForResource:language ofType:@"lproj"]] : nil, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+@end
 //--------------- Demo ---------------------------------
 
 static BOOL hasError;
@@ -230,15 +232,20 @@ static BOOL hasError;
 	
 	if ([_langlower containsString:@"en"]) {
 	  [[NSUserDefaults standardUserDefaults] setObject:[NSArray arrayWithObjects:@"en", nil] forKey:@"AppleLanguages"];
-	  //[NSBundle setLanguage:@"en"];
+	  [NSBundle setLanguage:@"en"];
 	  NSLog(@"en selected");
 	} else {
-	//	 [NSBundle setLanguage:@"ar"];
+	 [NSBundle setLanguage:@"ar"];
 	  NSLog(@"ar selected");
 	    [[NSUserDefaults standardUserDefaults] setObject:[NSArray arrayWithObjects:@"ar", nil] forKey:@"AppleLanguages"];
 	}
 
    [[NSUserDefaults standardUserDefaults] synchronize]; //to make the change immediate
+   
+	AppDelegate *delegate = [[UIApplication sharedApplication] delegate];
+    NSString *storyboardName = @"Main";
+    UIStoryboard *storybaord = [UIStoryboard storyboardWithName:storyboardName bundle:nil];
+    delegate.window.rootViewController = [storybaord instantiateInitialViewController];
    
    // refresh
    // NSArray *windows = [UIApplication sharedApplication].windows;
